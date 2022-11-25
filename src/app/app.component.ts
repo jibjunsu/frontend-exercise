abcb8e03026b887aac8c9ddc2cfdbe2d17da1fba
jibjunsu <jibjunsu@gmail.com>
2022-11-25 08:07:54 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { NxDialogService, NxModalRef } from '@aposin/ng-aquila/modal';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+})
+export class AppComponent {
+    @ViewChild('consentTemplate') consentTemplateRef!: TemplateRef<any>;
+    @ViewChild('submitTemplate') submitTemplateRef!: TemplateRef<any>;
+    dialogRef!: NxModalRef<any>;
+    formGroup: UntypedFormGroup;
+
+    constructor(readonly dialogService: NxDialogService) {
+        this.formGroup = new UntypedFormBuilder().group({
+            name: ['', Validators.required],
+            items: ['', Validators.required],
+            email: ['', [Validators.email, Validators.required]],
+            consent: [false, Validators.requiredTrue],
+        });
+    }
+
+    openConsentDialog(): void {
+        this.dialogRef = this.dialogService.open(this.consentTemplateRef, {
+            ariaLabel: 'A modal with content',
+            showCloseIcon: true,
+        });
+    }
+
+    openSubmitDialog(): void {
+        this.dialogRef = this.dialogService.open(this.submitTemplateRef, {
+            ariaLabel: 'The final modal of the Starter App',
+            showCloseIcon: false,
+        });
+    }
+
+    closeDialog() {
+        this.dialogRef.close();
+    }
+}
+
+/** Copyright Allianz 2022 */
